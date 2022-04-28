186e5dc7579afb89c5220a9f7d3f8b2091f8f680
glorianascimento <gllorianascimento@gmail.com>
2022-04-28 19:25:59 -0300

    exercicio condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,32 @@
+console.log("olá mundo");
+/* exercicio interretação
+1. a)
+O verifica se o numero que o usuario digitou é par ou não
+se for par o resultado é "passou no teste" se não "não passou no teste"
+b) 
+Numero pares
+c) 
+Numeros impares
+
+2. a)
+O código serve para mostrar o valor de uma fruta especifica
+b)
+O preço da fruta Maça é R$ 2.25
+c)
+O preço da fruta pêra é R$ 5 
+
+3.a)
+A primeira linha pede ao usuario um numero.
+b) 
+Essa mensgem é secreta!!!, vai da um erro
+c)
+Sim, pois a variavel só existe se a condição for verdadeira.
+*/
+// Exercicio Prático
+//1.a)
+const idadeDoUsuario = prompt ("Qual sua idade?")
+if (idadeDousuaroio > 18)) {
+    
+} else {
+    
+}
