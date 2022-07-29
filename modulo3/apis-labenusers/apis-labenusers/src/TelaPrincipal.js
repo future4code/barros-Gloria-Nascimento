bfba478e1620e499c1189dfee6d9c54d15d2d5ff
glorianascimento <gllorianascimento@gmail.com>
2022-07-28 22:11:35 -0300

    exercicio de apis

---
+++ modulo3/apis-labenusers/apis-labenusers/src/TelaPrincipal.js
@@ -0,0 +1,13 @@
+
+function Principal() {
+    return (
+        <div>
+            <button>Trocar de Tela</button>
+            <br></br>
+            <input placeholder='Nome'></input>
+            <input placeholder='E-mail'></input>
+            <button >Criar Usuário</button>        
+        </div>
+  );}
+  
+  export default Principal;
